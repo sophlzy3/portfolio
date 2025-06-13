67fe6b368ef08d6113ffbbde8fc9058f89a34f97
sophlzy3 <96968503+sophlzy3@users.noreply.github.com>
2025-06-13 00:31:51 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,13 +3,13 @@ const nextConfig = {
   output: "export",
   images: { unoptimized: true },
   trailingSlash: true,
-  basePath: process.env.NODE_ENV === "production" ? "/portfolio" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/portfolio" : "",
   // Only use distDir in production
-  ...(process.env.NODE_ENV === "production" ? {
-    distDir: 'dist',
-    cleanDistDir: true,
-  } : {}),
+  ...(process.env.NODE_ENV === "production"
+    ? {
+        distDir: "dist",
+        cleanDistDir: true,
+      }
+    : {}),
 };
 
-module.exports = nextConfig; 
+module.exports = nextConfig;
