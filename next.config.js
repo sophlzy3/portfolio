04837294e6837e493a5dd39010b978a49c4b4302
sophlzy3 <96968503+sophlzy3@users.noreply.github.com>
2025-06-13 00:37:35 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,13 +3,6 @@ const nextConfig = {
   output: "export",
   images: { unoptimized: true },
   trailingSlash: true,
-  // Only use distDir in production
-  ...(process.env.NODE_ENV === "production"
-    ? {
-        distDir: "dist",
-        cleanDistDir: true,
-      }
-    : {}),
 };
 
 module.exports = nextConfig;
